8e9953912a80573cbb4796c43bf136d89b3befb5
P-Draper <draperpa@gmail.com>
2023-05-13 13:26:14 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+    const moreCats = [...cats, "Broom"];
+    return moreCats;
+}
+function prependCat() {
+    const moreCats = ["Arnold", ...cats];
+    return moreCats;
+}
+function removeLastCat() {
+    const lessCats = cats.slice(0, cats.length - 1);
+    return lessCats;
+}
+function removeFirstCat() {
+    const lessCatsFront = cats.slice(1);
+    return lessCatsFront;
+}
+    
+
